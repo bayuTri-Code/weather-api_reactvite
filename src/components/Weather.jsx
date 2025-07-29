32259a6c4bf5138462819d0f2c48ca3e036ec156
bayu-Apps <bayurara555@gmail.com>
2025-07-29 14:27:30 +0700

    commit project setup API

---
+++ src/components/Weather.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import "./Weather.css";
+import searchIcon from "../assets/search.png";
+import clearIcon from "../assets/clear.png";
+import cloudIcon from "../assets/cloud.png";
+import drizzbleIcon from "../assets/drizzle.png";
+import humidityIcon from "../assets/humidity.png";
+import rainIcon from "../assets/rain.png";
+import snowIcon from "../assets/snow.png";
+import windIcon from "../assets/wind.png";
+
+const Weather = () => {
+
+    const [weatherData,setWeatherData] = useState(false);
+
+    const allIcon = {
+        "01d": clearIcon,
+        "01n": clearIcon,
+        "02d": cloudIcon,
+        "02n": cloudIcon,
+        "03d": cloudIcon,
+        "03n": cloudIcon,
+        "04d": drizzbleIcon,
+        "04n": drizzbleIcon,
+        "09d": rainIcon,
+        "09n": rainIcon,
+        "10d": rainIcon,
+        "10n": rainIcon,
+        "13d": snowIcon,
+        "13n": snowIcon,
+    }
+
+    const search = async (city)=>{
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_API_ID}`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            const icon = allIcon[data.weather[0].icon];
+            setWeatherData({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon,
+            });
+        } catch (error) {
+            alert("City not found");
+            console.error("Error fetching weather data:", error);
+        }
+    }
+
+    useEffect(()=>{
+        search("London");
+    },[]);
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img src={searchIcon} alt="" />
+      </div>
+      <img src={clearIcon} className="weather-icon" alt="" />
+      <p className="temperature">16°c</p>
+      <p className="location">London</p>
+      <div className="weather-data">
+        <div className="col">
+            <img src={humidityIcon} alt="" />
+            <div>
+                <p>91 %</p>
+                <span>Humidity</span>
+            </div>
+        </div>
+        <div className="col">
+            <img src={windIcon} alt="" />
+            <div>
+                <p>3.6 km/h</p>
+                <span>Wind Speed</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
